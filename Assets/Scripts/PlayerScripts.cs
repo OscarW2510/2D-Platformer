7efc761f711fcc5ad7b1818c5fc8afeaab1a5bea
OscarW1 <162868@chrome.escg.ac.uk>
2019-10-15 14:23:27 +0100

    Actual project

---
+++ Assets/Scripts/PlayerScripts.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScripts : MonoBehaviour{
+
+    public float jumpPower = 10.0f;
+    Rigidbody2D myRigidbody;
+    public bool isGrounded = false;
+
+    // Start is called before the first frame update
+    void Start(){
+        myRigidbody = transform.GetComponent<Rigidbody2D>();  
+    }
+
+   
+    void FixedUpdate() {
+        if (Input.GetKey(KeyCode.Space) && isGrounded) {
+            myRigidbody.AddForce(Vector3.up * (jumpPower * myRigidbody.mass * myRigidbody.gravityScale * 20.0f));
+        }
+    }
+
+
+    void OnCollisionEnter2D(Collision2D other){
+
+        if (other.collider.tag == "Ground"){
+            isGrounded = true;
+        }
+
+    }
+
+    void OnCollisionStay2D(Collision2D other)
+    {
+
+        if (other.collider.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+
+        if (other.collider.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+
+    }
+
+
+
+}
