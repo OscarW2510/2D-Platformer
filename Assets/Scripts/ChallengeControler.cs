3e3df7477efa639ff9542c78c1c64eb68c09adec
OscarW1 <162868@chrome.escg.ac.uk>
2019-10-21 15:16:29 +0100

    Added the movemnt

---
+++ Assets/Scripts/ChallengeControler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChallengeControler : MonoBehaviour{
+
+    public float scrollspeed = 5.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+        //Scrolling
+        GameObject currentChild;
+        for (int i = 0; i < transform.childCount; i++){
+            currentChild = transform.GetChild(i).gameObject;
+            ScrollChallenge(currentChild);
+            if (currentChild.transform.position.x <= -15.0f) {
+                Destroy(currentChild);
+
+            }
+        }
+
+    }
+
+    void ScrollChallenge(GameObject currentChallenge){
+        currentChallenge.transform.position -= Vector3.left * (scrollspeed * Time.deltaTime);
+    }
+
+
+}
