3e663bf6489bdde86fa5fa8b455c0db045d3a0d7
OscarW1 <162868@chrome.escg.ac.uk>
2019-11-11 18:41:17 +0000

    Got rid of the teleportation and added the jump pads back just need help on getting them to work

---
+++ Assets/Scripts/JumpPad.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPad : MonoBehaviour
+{
+    [SerializeField] float upwardBounce = 10f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+
+            Rigidbody playerRigidbody = other.gameObject.GetComponent<Rigidbody>();
+
+            if (playerRigidbody != null)
+            {
+
+                playerRigidbody.velocity = new Vector3(0, upwardBounce, 0);
+            }
+        }
+    }
+}
