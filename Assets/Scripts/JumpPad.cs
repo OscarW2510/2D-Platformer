e1eeae8bd4d3c496c473aa738435da2378ff9e82
OscarW1 <162868@chrome.escg.ac.uk>
2019-11-12 13:18:16 +0000

    Got my jump pad working and added somethings to the level

--- Assets/Scripts/JumpPad.cs
+++ Assets/Scripts/JumpPad.cs
@@ -6,18 +6,20 @@ public class JumpPad : MonoBehaviour
 {
     [SerializeField] float upwardBounce = 10f;
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (collision.gameObject.tag == "Player")
         {
 
-            Rigidbody playerRigidbody = other.gameObject.GetComponent<Rigidbody>();
+            Rigidbody2D playerRigidbody = collision.gameObject.GetComponent<Rigidbody2D>();
 
             if (playerRigidbody != null)
             {
-
+                
                 playerRigidbody.velocity = new Vector3(0, upwardBounce, 0);
             }
+           
         }
     }
-}
+
+} 
